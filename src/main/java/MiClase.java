bc2b273e49f8905e34ba3cdce2e1a28964f2413b
Oriel Delgado <oriel.delgado@utp.ac.pa>
2021-07-15 13:07:29 -0500

    Muestra

--- src/main/java/MiClase.java
+++ src/main/java/MiClase.java
@@ -1,14 +1,13 @@
 public class MiClase {
-    public static void main(String args[]) {
-        var a = 2;
-        if (a == 0) {
-            System.out.println("El valor de a es 0");
-        } else if (a == 1) {
-            System.out.println("El valor de a es 1");
-        } else if (a == 2){
-            System.out.println("El valor de a es 2");
-        } else{
-            System.out.println("El valor de a es desconocido");
+    public static boolean esPrimo(int numero){
+        int contador =2;
+        boolean primo = true;
+
+        while ((primo)&& (contador != numero)){
+            if (numero % contador ==0)
+            primo = false;
+            contador++;
         }
+        return primo;
     }
 }
