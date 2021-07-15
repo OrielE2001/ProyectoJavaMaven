259a6d2f9e04d4ff3b7589c98bfc52fe53f5b993
Oriel Delgado <oriel.delgado@utp.ac.pa>
2021-07-15 13:26:13 -0500

    Mas agragados

--- src/main/java/MiClase.java
+++ src/main/java/MiClase.java
@@ -1,14 +1,13 @@
 public class MiClase {
-    public static void burbuja (int []matrix){
-        int temporal;
-        for (int i=1; i < matrix.length; i++){
-            for (int j=0; j < matrix.length-1; j++){
-                if (matrix[j] > matrix[j+1]){
-                    temp = matrix[j];
-                    matrix[j]=matrix[j+1];
-                    matrix[j+1]=temp:
-                }
-            }
+    public static boolean esPrimo(int numero){
+        int contador =2;
+        boolean primo = true;
+
+        while ((primo)&& (contador != numero)){
+            if (numero % contador ==0)
+            primo = false;
+            contador++;
         }
+        return primo
     }
 }
